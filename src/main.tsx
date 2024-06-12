ec0b5cb997995a86ca10b800ae06d33630d89e13
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-06-12 17:33:23 -0600

    feat: add vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import '@fontsource-variable/dm-sans';
+import { Analytics } from "@vercel/analytics/react"
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>,
 )
